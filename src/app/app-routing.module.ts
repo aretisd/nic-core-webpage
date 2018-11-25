0731eedbfeffbdff383fc1c33ac1fff7cabbb4b2
aretisd9 <aretisd>
2018-11-26 00:40:13 +0700

    edit some route and add external link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, InjectionToken } from '@angular/core';
+import { RouterModule, Routes, ActivatedRouteSnapshot } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RedirectComponent } from './redirect/redirect.component';
+
+const externalUrlProvider = new InjectionToken('externalUrlRedirectResolver');
+const deactivateGuard = new InjectionToken('deactivateGuard');
 
 const routes: Routes = [
-    { path: 'home', component: HomepageComponent }
+    { path: '', component: HomepageComponent },
+    { path: 'externalRedirect', canActivate: [externalUrlProvider], component: RedirectComponent}
 ];
 
 @NgModule( {
     imports: [
         RouterModule.forRoot(routes)
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [
+        {
+            provide: externalUrlProvider,
+            useValue: (route: ActivatedRouteSnapshot) => {
+                const externalUrl = route.paramMap.get('externalUrl');
+                window.open(externalUrl, '_self');
+            },
+        },
+        {
+            provide: deactivateGuard,
+            useValue: () => {
+                console.log('Guard function is called!');
+                return false;
+            }
+        }
+    ]
 })
 export class AppRoutingModule {}
