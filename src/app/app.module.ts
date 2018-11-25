500a0b4d301ca88b2dbf9481460f159134e120ec
aretisd9 <aretisd>
2018-11-26 00:40:56 +0700

    add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,38 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { CallAPIService } from './service/call-api.service';
+import { APIService } from './service/api.service';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule, MatFormFieldModule, MatButtonModule, MatExpansionModule} from '@angular/material';
+import { RedirectComponent } from './redirect/redirect.component';
+import { ExternalUrlDirective } from './external-url.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    RedirectComponent,
+    ExternalUrlDirective
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
-  providers: [CallAPIService],
+  exports: [
+    MatExpansionModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+
+  ],
+  providers: [APIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
